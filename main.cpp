3192e47fdd0d5b06cca37067d1ecf59a46ce43ac
576565773@qq.com <576565773@qq.com>
2017-01-28 19:39:17 +0800

    organized files

--- main.cpp
+++ main.cpp
@@ -11,16 +11,18 @@ int main(int argc, char** argv)
 {
 	if(argc != 2)
 	{
-		cerr << "usage: ./hazeRemovalBin [image_directory]/[image_name].[image_file_format]";
+		cerr << "usage: ./hazeRemovalBin [image_directory]/[image_name].[image_file_format]" << endl;
 		return 0;
 	}
 
-	Mat img = imread("river.png");
+	string imgFile = argv[1];
+	Mat img = imread(imgFile);
 	imshow("oringal img",img);
 
 	HazeRemover hr;
 	hr.process(img);
 	imshow("result",hr.mDehazeImg);
+	
 	waitKey();
 	return 0;
 }
